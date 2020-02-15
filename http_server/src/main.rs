017271acc3adb1e1b9503745ee1f20bf684f3d57
Eli <e_corrales2003@yahoo.com>
2020-02-15 11:55:39 -0500

    .

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,12 +1,7 @@
 
 use std::net::TcpListener;
 use std::net::TcpStream;
-<<<<<<< HEAD
-=======
 use std::io::Read;
->>>>>>> 4001fa0dcb02d17d2704f97ba4eee03156b2e3bc
-use std::io::Write;
-use std::io::Error;
 
 const IP:&str = "localhost:8594";
 
@@ -29,12 +24,7 @@ fn main() {
 
             Ok(_stream) => {
                             println!("Handling request....{:?}", _stream);
-                            let resp_result = handle_client_request(_stream);
-                            println!("{:?}", resp_result);
-                            match resp_result {
-                                Ok(ok_var) => println!("{:?}", ok_var),
-                                Err(err_var) => println!("{:?}", err_var)
-                            }
+                            handle_client_request(_stream);
                         }
 
             _      => println!("There was an error...")
@@ -43,9 +33,25 @@ fn main() {
     }
 }
 
-fn handle_client_request(mut stream:TcpStream) -> Result<usize,Error> {
+fn handle_client_request(mut stream:TcpStream) {
     println!("Inside request handler: {:?}", stream);
-    let response = "HTTP/1.1 200 OK \r\n\r\n";
-    let result = stream.write(response.as_bytes());
-    return result;
+
+    let mut incoming_request_data = [0;32];
+    println!("{:?}", incoming_request_data);
+
+    let read_result = stream.read(&mut incoming_request_data);
+    match read_result {
+        Ok(ok_var) => println!("got Ok: {:?}", ok_var),
+        Err(err_var) => println!("got Err: {:?}", err_var)
+    }
+
+    let bytes_to_string = std::str::from_utf8(&incoming_request_data);
+
+    //println!("{:?}", incoming_request_data.unwrap());
+    println!("{:?}", incoming_request_data);
+    println!("{:?}", bytes_to_string.unwrap());
 }
+
+
+
+
