ff851cb4a4eda62ac2f082d39e13ca9b8e38973f
Eli <e_corrales2003@yahoo.com>
2020-02-16 22:29:26 -0500

    .

--- http_rocket_server/src/main.rs
+++ http_rocket_server/src/main.rs
@@ -1,12 +1,33 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 use rocket::*;
+use std::string::String;
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+static mut VALUE:u8 = 0;
+
+#[get("/value")]
+fn get_value() -> String {
+
+    unsafe {
+        println!("{}", VALUE); 
+        format!("GET value: {}", VALUE)
+    }
 }
 
+#[post("/value/<value>")]
+fn post_value(value:u8) -> String {
+
+    println!("{}", value); 
+
+    unsafe {
+        VALUE = value;
+        format!("PUT value {}", VALUE)
+    }
+
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    ignite()
+        .mount("/api",routes![get_value,post_value])
+        .launch();
 }
