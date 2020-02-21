82b940f4809807258103a6af43112b7da40253cb
Eli <e_corrales2003@yahoo.com>
2020-02-21 16:55:48 -0500

    .

--- serial-port/src/main.rs
+++ serial-port/src/main.rs
@@ -1,12 +1,11 @@
-
 use serialport::*;
 
 fn main() {
 
     match available_ports() {
 
-        Ok(ok_var) => println!("{:?}", ok_var),
-        Err(err_var) => println!("{:?}", err_var)
+        Ok(ok_var) => println!("Ok {:?}", ok_var.len()),
+        Err(err_var) => println!("Err {:?}", err_var)
 
     }
 
