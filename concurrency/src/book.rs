b1f6a90967d281d5b4f5960c056d67b5665f7ccb
Eli <e_corrales2003@yahoo.com>
2020-02-29 19:19:10 -0500

    .

--- concurrency/src/book.rs
+++ concurrency/src/book.rs
@@ -1,8 +1,28 @@
-
-pub struct Book {
-    id:u32  =  0,
-    title:String = "",
-    author:String = "",
-    year_published:u32 = 0
+pub struct Book<'a> {
+    pub id: u32,
+    pub author: &'a str
 }
+
+pub static BOOKS:[Book;20] = [
+    Book { id: 1, author: "Name1" },
+    Book { id: 2, author: "Name2" },
+    Book { id: 3, author: "Name3" },
+    Book { id: 4, author: "Name4" },
+    Book { id: 5, author: "Name5" },
+    Book { id: 6, author: "Name6" },
+    Book { id: 7, author: "Name7" },
+    Book { id: 8, author: "Name8" },
+    Book { id: 9, author: "Name9" },
+    Book { id: 10, author: "Name10" },
+    Book { id: 11, author: "Name11" },
+    Book { id: 12, author: "Name12" },
+    Book { id: 13, author: "Name13" },
+    Book { id: 14, author: "Name14" },
+    Book { id: 15, author: "Name15" },
+    Book { id: 16, author: "Name16" },
+    Book { id: 17, author: "Name17" },
+    Book { id: 18, author: "Name18" },
+    Book { id: 19, author: "Name19" },
+    Book { id: 20, author: "Name20" },
+];
 
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,14 +1,42 @@
 mod book;
-
-use lazy_static::lazy_static;
-use std::sync::Mutex;
-
-
-
-lazy_static! {
-    static ref cache:Mutex<book::Book> = Mutex::new(book::Book{});
-}
+use book::*;
 
 
 fn main() {
+
+    let mut cache:Vec<&Book> = Vec::new();
+
+    query_cache(&cache);
+
+    query_database(&mut cache);
+
+    query_cache(&cache);
+}
+
+fn query_cache(cache:&Vec<&Book>) {
+
+    println!("|Query Cache----------------------------------|");
+    println!("|                                             |");
+
+    for book in cache {
+        println!("| {} {}", book.id, book.author);
+    }
+    println!("|                                             |");
+    println!("-----------------------------------------------");
+}
+
+
+fn query_database(cache:&mut Vec<&Book>) {
+
+    println!("Query Database-------------------------------");
+    println!("|                                             |");
+
+    for i in 0..20 {
+        let id = BOOKS[i].id;
+        let author = BOOKS[i].author;
+        println!("| {} {}", id, author);
+        cache.push(&BOOKS[i]);
+    }
+    println!("|                                             |");
+    println!("-----------------------------------------------");
 }
