55482afdfa3f26b159e4b849809336fa011fb96b
Eli <e_corrales2003@yahoo.com>
2020-03-03 15:31:50 -0500

    .

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -2,23 +2,35 @@ use std::thread::spawn;
 use std::thread::sleep;
 use std::time::Duration;
 
+static mut some_extern_var:u32 = 10;
+
 fn main() {
 
-    let my_thread = |num|{
-        sleep(Duration::from_millis(1000));
-        println!("Thread {}", num);
+    //let mut some_extern_var:u32 = 10;
+    //let ref_to_ext_var = &some_extern_var;
+
+    let my_thread_using_extern_vars = move ||{
+        unsafe {
+        sleep(Duration::from_millis(500));
+        println!("Thread using extern");
+        println!("in thread: {}", some_extern_var);
+        //println!("in thread: {}", ref_to_ext_var);
         println!("");
+        some_extern_var = 11;
+        }
     };
 
-    println!("");
-    println!("Main thread starts other threads..");
+ 
+    spawn(my_thread_using_extern_vars);
 
-    my_thread(1);
-    my_thread(2);
+    unsafe {
+    println!("In main: {}", some_extern_var);
+    }
 
-    spawn(my_thread(1));
-    spawn(my_thread(2));
-
+    sleep(Duration::from_millis(1200));
+    unsafe {
+    println!("In main: {}", some_extern_var);
+    }
 
     println!("");
     println!("Main thread waiting...");
