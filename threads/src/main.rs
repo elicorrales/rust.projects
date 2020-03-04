da7ddc77cf64b1ec761f24c751a666666a820437
Eli <e_corrales2003@yahoo.com>
2020-03-04 15:50:36 -0500

    .

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,41 +1,49 @@
 use std::thread::spawn;
 use std::thread::sleep;
 use std::time::Duration;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
+use std::io::Write;
 
 
-lazy_static! {
-    static ref SOME_EXTERN_VAR:Mutex<u32> = Mutex::new(10);
-}
+static mut SOME_EXTERN_VAR:u32 = 0;
 
 
 fn main() {
 
-    let my_thread_using_extern_vars = ||{
+    let my_thread_1 = |val|{
+        //print!("1 ");
+        unsafe {
+            SOME_EXTERN_VAR = 11;
+        }
         sleep(Duration::from_millis(500));
-        println!("Thread using extern");
-        println!("in thread: {:?}", SOME_EXTERN_VAR.lock().unwrap());
-        let mut ext = SOME_EXTERN_VAR.lock().unwrap();
-        println!("in thread: {:?}", ext);
-        *ext = 11;
-        println!("in thread: {:?}", ext);
-        println!("");
+        unsafe {
+            if SOME_EXTERN_VAR != 11 {
+                //print!("E1 ");
+            } 
+        }
+        std::io::stdout().flush().unwrap();
     };
 
- 
-    spawn(my_thread_using_extern_vars);
+    let my_thread_2 = |val|{
+        //print!("2 ");
+        unsafe {
+            SOME_EXTERN_VAR = 22;
+        }
+        sleep(Duration::from_millis(500));
+        unsafe {
+            if SOME_EXTERN_VAR != 22 {
+                //print!("E2 ");
+            }
+        }
+        std::io::stdout().flush().unwrap();
+    };
 
-    //this insures that thread grabs extern first and modifies it.
-    //if this line is commented, main might grab ext first 
-    sleep(Duration::from_millis(1200));
 
-    let ext = SOME_EXTERN_VAR.lock().unwrap();
-    println!("In main: {}", ext);
+    for i in 0..40 {
+        sleep(Duration::from_millis(80));
+        spawn(my_thread_1(11));
+        spawn(my_thread_2(22));
+    }
 
-    sleep(Duration::from_millis(1200));
-
-    println!("In main: {}", ext);
 
     println!("");
     println!("Main thread waiting...");
