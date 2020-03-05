11917110064c2902ca8bad55d59efc365c50df1c
Eli <e_corrales2003@yahoo.com>
2020-03-05 13:06:56 -0500

    .

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,27 +1,50 @@
 use std::thread::sleep;
 use std::thread::spawn;
 use std::time::Duration;
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref NUM:Mutex<usize>  = Mutex::new(10);
+}
+
 
 fn main() {
 
     let task1 = || {
-        sleep(Duration::from_millis(200));
-        println!("Task 1 is done.");
+        let mut rnum = NUM.lock().unwrap();
+        *rnum = 11;
+        sleep(Duration::from_millis(400));
+        if *rnum != 11 {
+            print!("E1 ");
+        }
     };
 
     let task2 = || {
-        sleep(Duration::from_millis(200));
-        println!("Task 2 is done.");
+        let mut rnum = NUM.lock().unwrap();
+        *rnum = 22;
+        sleep(Duration::from_millis(400));
+        if *rnum != 22 {
+            print!("E2 ");
+        }
     };
+
 
     println!("Main is starting....");
 
-    spawn(task1);
-    spawn(task2);
+
+    for i in 0..20 {
+        spawn(task1);
+        spawn(task2);
+    }
 
     println!("Main is waiting....");
 
-    sleep(Duration::from_millis(500));
+    sleep(Duration::from_millis(1000));
+
+
+    println!("");
+    println!("In Main: {}", NUM.lock().unwrap());
 
     println!("Main is done");
 }
